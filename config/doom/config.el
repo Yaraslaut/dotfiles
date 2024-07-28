d2ea17333f2282764680db9861bf170ac1ca0664
Yaraslau <yaraslau.tamashevich@gmail.com>
2024-07-28 14:03:10 +0300

    update config from green

--- config/doom/config.el
+++ config/doom/config.el
@@ -11,9 +11,10 @@
 (cond ( (equal system-name "Green.local") )
       ( t
         (setq doom-font (font-spec :family "MonaspiceKr Nerd Font" :size me/doom-font-size))
-        (setq doom-theme 'doom-acario-dark)
         )
 )
+
+(setq doom-theme 'doom-acario-dark)
 
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
@@ -24,14 +25,16 @@
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 
-(setq system-clangd "/usr/bin/clangd")
+(cond
+     ( (equal system-name "Green.local") (setq system-clangd "/opt/homebrew/opt/llvm/bin/clangd") )
+     ( t  (setq system-clangd "/usr/bin/clangd") )
+)
 
 (use-package! lsp  ; `use-package!' is a thin wrapper around `use-package'
                        ; it is required that you use this in Doom's modules,
                        ; but not required to be used in your private config.
   :config
   (setq
-
    lsp-clients-clangd-executable system-clangd
    lsp-clients-clangd-args '("--header-insertion-decorators=0" "--pch-storage=disk" "--clang-tidy")
 ))
