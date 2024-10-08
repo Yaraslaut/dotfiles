f301481b986c99352e880316fbd6e1f11156d198
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2024-10-08 14:39:46 +0300

    add tabs mapping for contour

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files '("~/repo/Unsorted/daniela/calendar/current.org"))
  '(package-selected-packages '(jsonrpc qml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
