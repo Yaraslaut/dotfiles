1de409d67673e9571446759f5c502f6b60bb954d
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2024-10-29 11:22:26 +0200

    Update doom

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,27 +1,26 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+(setq doom-theme 'doom-acario-dark)
 (setq doom-font-increment 1)
 (setq me/doom-font-size 14)
 
 
-;;; check system name
-
-
-;;; only for not Green.local
-(cond ( (equal system-name "bahamankolibri.lin.tuni.fi")
-        (setq copilot-node-executable "/worktmp/yaraslau/programs/node-v20.12.2-linux-x64/bin/node") )
-)      
-
-
-(cond ( (equal system-name "Green.local") )
-      ( t
-        (setq doom-font (font-spec :family "MonaspiceKr Nerd Font" :size me/doom-font-size))
-        )
+;; set clang executable for different systems
+;; Green.local - > "/opt/homebrew/opt/llvm/bin/clangd"
+;; bahamankolibri.lin.tuni.fi -> "/usr/bin/clangd"
+;; Cartan -> /home/yaraslau/.local/llvm-19/bin/clangd
+(setq system-clang "/usr/bin/clang")
+(cond
+     ( (equal (system-name) "Green.local")                (setq system-clangd "/opt/homebrew/opt/llvm/bin/clangd") )
+     ( (equal (system-name) "bahamankolibri.lin.tuni.fi") (setq system-clangd "/usr/bin/clangd") )
+     ( (equal (system-name) "Cartan" )                    (setq system-clangd "/home/yaraslau/.local/llvm-19/bin/clangd") )
 )
 
+;;; only for not Green.local
+(cond ( (equal (system-name) "Green.local") () )
+      ( t (add-hook 'after-save-hook 'lsp-format-buffer))
+)
 
-
-(setq doom-theme 'doom-acario-dark)
 
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
@@ -31,28 +30,22 @@
               ("C-TAB" . 'copilot-accept-completion-by-word)
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
-
-
-(cond
-     ( (equal system-name "Green.local") (setq system-clangd "/opt/homebrew/opt/llvm/bin/clangd") )
-     ( t  (setq system-clangd "/usr/bin/clangd") )
-)
-
-(use-package! lsp  ; `use-package!' is a thin wrapper around `use-package'
-                       ; it is required that you use this in Doom's modules,
-                       ; but not required to be used in your private config.
+(use-package! lsp
   :config
   (setq
    lsp-clients-clangd-executable system-clangd
-   lsp-clients-clangd-args '("--header-insertion-decorators=0" "--pch-storage=disk" "--clang-tidy")
-))
+   lsp-clients-clangd-args '("--header-insertion-decorators=0" "--pch-storage=disk" "--clang-tidy")))
 
+(cond ( (equal (system-name) "bahamankolibri.lin.tuni.fi")
+        (setq copilot-node-executable "/worktmp/yaraslau/programs/node-v20.12.2-linux-x64/bin/node") )
+)      
 
-
-;;; only for not Green.local
-(cond ( (equal system-name "Green.local") )
-      ( t (add-hook 'after-save-hook 'lsp-format-buffer))
+(cond ( (equal (system-name) "Green.local") )
+      ( t
+        (setq doom-font (font-spec :family "MonaspiceKr Nerd Font" :size me/doom-font-size))
+        )
 )
+
 
 
 
@@ -67,5 +60,4 @@
           (insert open)))
     (insert open close))
   (backward-char))
-
 ;(global-set-key (kbd "C-c e") (lambda() (interactive) (insert-quotes  "\"" "\\n\"")))
--- config/doom/init.el
+++ config/doom/init.el
@@ -23,9 +23,7 @@
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
